35f2069145db7d0904e2cdaa1d90d1dad65267d4
Priyanshu Kumar Sharma <118095177+PriyanshuKSharma@users.noreply.github.com>
2024-09-19 13:33:08 +0530

    Added cmds to check permissions for the file

--- r-w-x-setup.sh
+++ r-w-x-setup.sh
@@ -8,3 +8,9 @@ ls -lh file1.txt
 
 # Set permissions using absolute method
 chmod 754 file1.txt
+
+# Check the permissions
+echo "Permissions:"
+ls -l file1.txt
+echo "Permissions in numerical format:"
+stat -c %a file1.txt
