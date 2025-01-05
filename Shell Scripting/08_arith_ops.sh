6f43bb491ceeb002ecf05bd095a4a3c76ae18b23
Priyanshu Kumar Sharma <118095177+PriyanshuKSharma@users.noreply.github.com>
2025-01-05 13:25:09 +0530

    Arithmetic Operations in Shell

---
+++ Shell Scripting/08_arith_ops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+x=10
+y=2
+
+#mul=$x*$y
+#echo "$mul" #Wrong way to perform arithmetic problems
+
+let mul=$x*$y
+echo "Product is: $mul"
+
+#Another way to perform arithmetic operations
+sum=$((x+y))
+echo "Sum is: $sum"
+
+difference=$(($x-$y))
+echo "Difference is: $difference"
