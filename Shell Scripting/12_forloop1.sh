97819de2d2cc83a0d29be8f7ad1d8e50138109e5
Priyanshu Kumar Sharma <118095177+PriyanshuKSharma@users.noreply.github.com>
2025-01-08 14:03:49 +0530

    For Loop

---
+++ Shell Scripting/12_forloop1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#For Loop
+for i in 1 2 3 4 5
+do 
+    echo $i
+done
+
+#Loop in Strings
+for j in Raju Sham Baburao
+do 
+    echo $j
+done
+
+#Loop in a range
+for i in {1..10}
+do
+    echo $i
+done
